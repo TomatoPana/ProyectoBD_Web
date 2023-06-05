46589bfd9fda6b1671fdb0d36b2d768e9684b202
JanneHerrera <nitto1257@gmail.com>
2023-06-05 01:01:23 -0600

    vista de lista

---
+++ lista.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
+    <title>Iniciar Sesión</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+</head>
+
+<body>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/index.php"><img src="/img/gameflix.jpg" height="30px"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/index.php">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/nosotros.php">Nosotros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/catalogo.php">Catálogo</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+    <div class="container">
+        <div class="row">
+            <div class="contenedor-lista">
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <span class="glyphicon glyphicon-list"></span>Videojuegos disponibles
+                    </div>
+                    <div class="panel-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">
+                                <label for="checkbox">
+                                    List group item heading
+                                </label>
+                                <div class="pull-right action-buttons">
+                                    <a href="/eliminar.php"><span class="glyphicon glyphicon-pencil"></span></a>
+                                    <a href="/eliminar.php" class="trash"><span class="glyphicon glyphicon-trash"></span></a>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <label for="checkbox">
+                                        List group item heading
+                                </label>
+                                <div class="pull-right action-buttons">
+                                    <a href="/eliminar.php"><span class="glyphicon glyphicon-pencil"></span></a>
+                                    <a href="/eliminar.php" class="trash"><span class="glyphicon glyphicon-trash"></span></a>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <label for="checkbox">
+                                    List group item heading
+                                </label>
+                                <div class="pull-right action-buttons">
+                                    <a href="/eliminar.php"><span class="glyphicon glyphicon-pencil"></span></a>
+                                    <a href="/eliminar.php" class="trash"><span class="glyphicon glyphicon-trash"></span></a>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <label for="checkbox">
+                                    List group item heading
+                                </label>
+                                <div class="pull-right action-buttons">
+                                    <a href="/eliminar.php"><span class="glyphicon glyphicon-pencil"></span></a>
+                                    <a href="/eliminar.php" class="trash"><span class="glyphicon glyphicon-trash"></span></a>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <label for="checkbox">
+                                    List group item heading
+                                </label>
+                                <div class="pull-right action-buttons">
+                                    <a href="/eliminar.php"><span class="glyphicon glyphicon-pencil"></span></a>
+                                    <a href="/eliminar.php" class="trash"><span class="glyphicon glyphicon-trash"></span></a>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <label for="checkbox">
+                                    List group item heading
+                                </label>
+                                <div class="pull-right action-buttons">
+                                    <a href="/eliminar.php"><span class="glyphicon glyphicon-pencil"></span></a>
+                                    <a href="/eliminar.php" class="trash"><span class="glyphicon glyphicon-trash"></span></a>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <label for="checkbox">
+                                    List group item heading
+                                </label>
+                                <div class="pull-right action-buttons">
+                                    <a href="/eliminar.php"><span class="glyphicon glyphicon-pencil"></span></a>
+                                    <a href="/eliminar.php" class="trash"><span class="glyphicon glyphicon-trash"></span></a>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <label for="checkbox">
+                                    List group item heading
+                                </label>
+                                <div class="pull-right action-buttons">
+                                    <a href="/eliminar.php"><span class="glyphicon glyphicon-pencil"></span></a>
+                                    <a href="/eliminar.php" class="trash"><span class="glyphicon glyphicon-trash"></span></a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="panel-footer">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h6>
+                                    Total de videojuegos  <span class="label label-info">#dvideojuegos</span></h6>
+                            </div>
+                            <div class="col-md-6">
+                                <ul class="pagination pagination-sm pull-right">
+                                    <li class="disabled"><a href="javascript:void(0)">«</a></li>
+                                    <li class="active"><a href="javascript:void(0)">1 <span class="sr-only">(current)</span></a></li>
+                                    <li><a href="#">2</a></li>
+                                    <li><a href="#">3</a></li>
+                                    <li><a href="#">4</a></li>
+                                    <li><a href="#">5</a></li>
+                                    <li><a href="javascript:void(0)">»</a></li> 
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
